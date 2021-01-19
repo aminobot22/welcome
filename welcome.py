ba6045a016f2d9bf4b6402b3275015e36af977f8
aminobot22 <76242458+aminobot22@users.noreply.github.com>
2021-01-19 15:04:14 +0530

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,37 @@
+import amino
+import time
+
+email = "gbad94611@gmail.com"  # Set your own password here!
+password = "7060187782"  # Set your own password here!
+cid = "226547416"  # Community ID
+
+client = amino.Client()
+client.login(email=email, password=password)
+print("Bot logged in")
+sub = amino.SubClient(comId=cid, profile=client.profile)
+print("Bot logged onto the community, id:", cid, "\nBot Name:", sub.profile.nickname)
+
+
+join = """welcome in the chat"""
+
+@client.callbacks.event('on_group_member_join')
+def on_group_member_join(data):
+        nick = data.message.author.nickname
+        msg = {
+        'message': f"{join}\n\n[CB]{nick} welcome 🌹🌹🌹🌹!!",
+        'chatId': data.message.chatId,
+        'mentionUserIds': [data.message.author.userId]
+        }
+        sub.send_message(**msg)
+def socketRoot( ) :
+	j = 0
+	while True:
+		if j >= 30:
+			print("updating socket...")
+			client.socket.close( )
+			client.socket.start( )
+			print("updated socket!")
+			j = 0
+		j += 1
+		time.sleep(1)
+socketRoot( )			        
